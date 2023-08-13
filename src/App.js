40fba9a8b3f45b987a7a5d66da3a20b7c354482f
Sourav Tiwari <souravtiwari@Souravs-MacBook-Pro.local>
2023-08-14 01:45:03 +0530

    added folder component in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { useMemo } from "react";
+import { SAMPLE_FILE } from "./constants";
+import { insertFile } from "./utils";
+import Folder from "./Folder";
+import "./index.css";
+
 function App() {
-  return <div>Test</div>;
+  const modifiedSampleFile = useMemo(() => {
+    let obj = { id: 0, name: "root", isDir: true, items: [] };
+    SAMPLE_FILE.forEach((file) => {
+      obj = insertFile(obj, file);
+    });
+    return obj;
+  }, []);
+  return (
+    <div className="App">
+      <Folder fileStructure={modifiedSampleFile} />
+    </div>
+  );
 }
 
 export default App;
