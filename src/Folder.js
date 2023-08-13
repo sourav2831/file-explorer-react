8ba7e205025eb4530679a513fb2145acd74af676
Sourav Tiwari <souravtiwari@Souravs-MacBook-Pro.local>
2023-08-14 01:44:50 +0530

    added folder component

---
+++ src/Folder.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Folder({ fileStructure }) {
+  const [expand, setExpand] = useState(false);
+
+  if (fileStructure.isDir) {
+    return (
+      <div style={{ marginTop: 5 }}>
+        <div className="folder">
+          <span>📁 {fileStructure.name}</span>
+          <span onClick={() => setExpand(true)}>+</span>
+          <span onClick={() => setExpand(false)}>-</span>
+        </div>
+        <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
+          {fileStructure.items.map((exp) => {
+            return <Folder key={exp.id} fileStructure={exp} />;
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return <span className="file">📄 {fileStructure.name}</span>;
+  }
+}
+
+export default Folder;
